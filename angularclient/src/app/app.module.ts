c715a47a466a1e5d270dd0dcc1095856ad040c68
leooo729 <79959043+leooo729@users.noreply.github.com>
2022-08-23 16:07:37 +0800

    Add files via upload

---
+++ angularclient/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from '../app/service/user-service.service';
+import { UpdateUserComponent } from './update-user/update-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    UserFormComponent,
+    UpdateUserComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
